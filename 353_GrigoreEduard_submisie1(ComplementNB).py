9d6910f757e7fefe392640ea53c1fa38230f1935
Eduard Grigore <62159078+griged99@users.noreply.github.com>
2020-12-14 19:25:22 +0200

    Add files via upload

---
+++ 353_GrigoreEduard_submisie1(ComplementNB).py
@@ -0,0 +1,174 @@
+import string
+import numpy as np
+from collections import Counter
+import pandas as pd
+import re
+import nltk
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.naive_bayes import ComplementNB
+
+
+def remove_punct(text):
+    text  = "".join([char for char in text if char not in string.punctuation])
+    text = re.sub('[0-9]+', '', text)
+    return text
+
+def tokenization(text):
+    text = re.split('\W+', text)
+    return text
+
+stopword = ["","a","abbastanza","abbia","abbiamo","abbiano","abbiate","accidenti","ad","adesso","affinché","agl","agli","ahime","ahimè","ai","al","alcuna","alcuni","alcuno","all","alla","alle","allo","allora","altre","altri","altrimenti","altro","altrove","altrui","anche","ancora","anni","anno","ansa","anticipo","atus","assai","attesa","attraverso","avanti","avemmo","avendo","avente","aver","avere","averlo","avesse","avessero","avessi","avessimo","aveste","avesti","avete","aveva","avevamo","avevano","avevate","avevi","avevo","avrai","avranno","avrebbe","avrebbero","avrei","avremmo","avremo","avreste","avresti","avrete","avrà","avrò","avuta","avute","avuti","avuto","basta","ben","bene","benissimo","brava","bravo","buono","c","caso","cento","certa","certe","certi","certo","che","chi","chicchessia","chiunque","ci","ciascuna","ciascuno","cima","cinque","cio","cioe","cioè","circa","citta","città","ciò","co","codesta","codesti","codesto","cogli","coi","col","colei","coll","coloro","colui","come","cominci","comprare","comunque","con","concernente","conclusione","consecutivi","consecutivo","consiglio","contro","cortesia","cos","cosa","cosi","così","cui","d","da","dagl","dagli","dai","dal","dall","dalla","dalle","dallo","dappertutto","davanti","degl","degli","dei","del","dell","della","delle","dello","dentro","detto","deve","devo","di","dice","dietro","dire","dirimpetto","diventa","diventare","diventato","dopo","doppio","dov","dove","dovra","dovrà","dovunque","due","dunque","durante","e","ebbe","ebbero","ebbi","ecc","ecco","ed","effettivamente","egli","ella","entrambi","eppure","era","erano","eravamo","eravate","eri","ero","esempio","esse","essendo","esser","essere","essi","ex","fa","faccia","facciamo","facciano","facciate","faccio","facemmo","facendo","facesse","facessero","facessi","facessimo","faceste","facesti","faceva","facevamo","facevano","facevate","facevi","facevo","fai","fanno","farai","faranno","fare","farebbe","farebbero","farei","faremmo","faremo","fareste","faresti","farete","farà","farò","fatto","favore","fece","fecero","feci","fin","finalmente","finche","fine","fino","forse","forza","fosse","fossero","fossi","fossimo","foste","fosti","fra","frattempo","fu","fui","fummo","fuori","furono","futuro","generale","gente","gia","giacche","giorni","giorno","giu","già","gli","gliela","gliele","glieli","glielo","gliene","grande","grazie","gruppo","ha","haha","hai","hanno","ho","i","ie","ieri","il","improvviso","in","inc","indietro","infatti","inoltre","insieme","intanto","intorno","invece","io","l","la","lasciato","lato","le","lei","li","lo","lontano","loro","lui","lungo","luogo","là","ma","macche","magari","maggior","mai","male","malgrado","malissimo","me","medesimo","mediante","meglio","meno","mentre","mesi","mezzo","mi","mia","mie","miei","mila","miliardi","milioni","minimi","mio","modo","molta","molti","moltissimo","molto","momento","mondo","ne","negl","negli","nei","nel","nell","nella","nelle","nello","nemmeno","neppure","nessun","nessuna","nessuno","niente","no","noi","nome","non","nondimeno","nonostante","nonsia","nostra","nostre","nostri","url","nostro","novanta","nove","nulla","nuovi","nuovo","o","od","oggi","ogni","ognuna","ognuno","oltre","oppure","ora","ore","osi","ossia","ottanta","otto","paese","parecchi","parecchie","parecchio","parte","partendo","peccato","peggio","per","perche","perchè","perché","percio","perciò","perfino","pero","persino","persone","però","piedi","pieno","piglia","piu","piuttosto","più","po","pochissimo","poco","poi","poiche","possa","possedere","posteriore","posto","potrebbe","preferibilmente","presa","press","prima","primo","principalmente","probabilmente","promesso","proprio","puo","pure","purtroppo","può","qua","qualche","qualcosa","qualcuna","qualcuno","quale","quali","qualunque","quando","quanta","quante","quanti","quanto","quantunque","quarto","quasi","quattro","quel","quella","quelle","quelli","quello","quest","questa","queste","questi","questo","qui","quindi","quinto","realmente","recente","recentemente","registrazione","relativo","riecco","rispetto","salvo","sara","sarai","saranno","sarebbe","sarebbero","sarei","saremmo","saremo","sareste","saresti","sarete","sarà","sarò","scola","scopo","scorso","se","secondo","seguente","seguito","sei","sembra","sembrare","sembrato","sembrava","sembri","sempre","senza","sette","si","sia","siamo","siano","siate","siete","sig","solito","solo","soltanto","sono","sopra","soprattutto","sotto","spesso","sta","stai","stando","stanno","starai","staranno","starebbe","starebbero","starei","staremmo","staremo","stareste","staresti","starete","starà","starò","stata","state","stati","stato","stava","stavamo","stavano","stavate","stavi","stavo","stemmo","stessa","stesse","stessero","stessi","stessimo","stesso","steste","stesti","stette","stettero","stetti","stia","stiamo","stiano","stiate","sto","su","sua","subito","successivamente","successivo","sue","sugl","sugli","sui","sul","sull","sulla","sulle","sullo","suo","suoi","tale","tali","talvolta","tanto","te","tempo","terzo","th","ti","titolo","tra","tranne","tre","trenta","triplo","troppo","trovato","tu","tua","tue","tuo","tuoi","tutta","tuttavia","tutte","tutti","tutto","uguali","ulteriore","ultimo","un","una","uno","uomo","va","vai","vale","vari","varia","varie","vario","verso","vi","vicino","visto","vita","voi","volta","volte","vostra","vostre","vostri","vostro","è"]
+
+def remove_stopwords(text):
+    text = [word for word in text if word not in stopword]
+    return text
+ps = nltk.PorterStemmer()
+
+def stemming(text):
+    text = [ps.stem(word) for word in text]
+    return text
+
+wn = nltk.WordNetLemmatizer()
+
+def lemmatizer(text):
+    text = [wn.lemmatize(word) for word in text]
+    return text
+
+
+def preprocess(text):
+    text = text.lower()  # convert text to lower-case
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', text) # remove URLs
+    text = re.sub('@[^\s]+', 'AT_USER', text)  # remove usernames
+    text = re.sub(r'#([^\s]+)', r'\1', text)  # remove the # in #hashtag
+    text_lc = "".join([word.lower() for word in text if word not in string.punctuation]) # remove puntuation
+    text_rc = re.sub('[0-9]+', '', text_lc)
+    tokens = re.split('\W+', text_rc)    # tokenization
+    text = [ps.stem(word) for word in tokens if word not in stopword]  # remove stopwords and stemming
+    return text
+
+def get_corpus_vocabulary(corpus):
+    '''Write a function to return all the words in a corpus.
+    '''
+    counter = Counter()
+    for text in corpus:
+        tokens = preprocess(text)
+        counter.update(tokens)
+    return counter
+
+
+def get_representation(toate_cuvintele, how_many):
+    '''Extract the first most common words from a vocabulary
+    and return two dictionaries: word to index and index to word
+    wd2idx     @  che  .   ,   di  e
+    idx2wd     0   1   2   3   4   5
+    '''
+    most_comm = toate_cuvintele.most_common(how_many)
+    wd2idx = {}
+    idx2wd = {}
+    for idx, itr in enumerate(most_comm):
+        cuvant = itr[0]
+        wd2idx[cuvant] = idx
+        idx2wd[idx] = cuvant
+    return wd2idx, idx2wd
+
+
+def text_to_bow(text, wd2idx):
+    '''Convert a text to a bag of words representation.
+           @  che  .   ,   di  e
+           0   1   2   3   4   5
+    text   0   1   0   2   0   1
+    '''
+    features = np.zeros(len(wd2idx))
+    for token in preprocess(text):
+        if token in wd2idx:
+            features[wd2idx[token]] += 1
+    return features
+
+
+def corpus_to_bow(corpus, wd2idx):
+    '''Convert a corpus to a bag of words representation.
+           @  che  .   ,   di  e
+           0   1   2   3   4   5
+    text0  0   1   0   2   0   1
+    text1  1   2 ...
+    ...
+    textN  0   0   1   1   0   2
+    '''
+    all_features = np.zeros((len(corpus), len(wd2idx)))
+    for i, text in enumerate(corpus):
+        all_features[i] = text_to_bow(text, wd2idx)
+    return all_features
+
+
+def write_prediction(out_file, predictions):
+    '''A function to write the predictions to a file.
+    id,label
+    5001,1
+    5002,1
+    5003,1
+    ...
+    '''
+    with open(out_file, 'w') as fout:
+        # aici e open in variabila 'fout'
+        fout.write('id,label\n')
+        start_id = 5001
+        for i, pred in enumerate(predictions):
+            linie = str(i + start_id) + ',' + str(pred) + '\n'
+            fout.write(linie)
+    # aici e fisierul closed
+
+
+def split(data, labels, procentaj_valid=0.25):
+    '''Split data and labels into train and valid by procentaj_valid.
+    75% train, 25% valid
+    Important! shuffle the data before splitting.
+    '''
+    indici = np.arange(len(labels))
+    np.random.seed(indici)
+    dim_train = round((1 - procentaj_valid) * len(labels))
+    train = data[indici][:dim_train]
+    y_train = labels[indici][:dim_train]
+    valid = data[indici][dim_train:]
+    y_valid = labels[indici][dim_train:]
+    return train, valid, y_train, y_valid
+
+
+train_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
+corpus = train_df['text']
+
+toate_cuvintele = get_corpus_vocabulary(corpus)
+wd2idx, idx2wd = get_representation(toate_cuvintele, 150)
+
+
+data = corpus_to_bow(corpus, wd2idx)
+labels = train_df['label'].values
+
+
+test_data = corpus_to_bow(test_df['text'], wd2idx)
+
+
+sub_df =pd.read_csv('submission.csv')
+labels_test=sub_df['label']
+
+
+clf = ComplementNB()
+
+x=data
+y=labels
+
+x_train, x_test, y_train, y_test = split(x,y)
+
+clf.fit(x_train,y_train)
+
+y_pred = clf.predict(x_test)
+prediction = clf.predict(x_test)
+prediction_train = clf.predict(x_train)
+
+print(f"Training Set Accuracy : {accuracy_score(y_train, prediction_train) * 100} %\n")
+print(f"Test Set Accuracy : {accuracy_score(y_test, prediction) * 100} % \n\n")
+print(f"Classifier Report : \n\n {classification_report(y_test, prediction)}")
+
+clf.fit(data,labels)
+predictii = clf.predict(test_data)
+write_prediction('submission.csv', predictii)
